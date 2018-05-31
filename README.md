0c7ea319c0196a1ae3c91ad56c3db3c752f7d432
c-daniel14 <cojocarugeani@yahoo.com>
2018-05-31 22:23:29 +0100

    uploading files

---
+++ README.md
@@ -0,0 +1,13 @@
+#Face Recognition App
+
+Application Server.
+
+Built with:
+- Clarifai API
+- Node.JS
+- Express.JS
+- Knex
+- Cors
+- PostgreSQL
+- bcrypt-nodejs
+- nodemon
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'Cojocaru14',
+    database : 'smart-brain'
+  }
+});
+
+db.select('*').from('users').then(data =>{
+	console.log(data);
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get("/", (req, res) =>{
+	res.send(database.users)
+});
+
+app.post("/signin",(req,res)=>{
+	signin.handleSignin(req,res,db,bcrypt)
+});
+
+app.post("/register",(req,res) => {
+	register.handleRegister(req, res, db, bcrypt)
+});
+
+app.get("/profile/:id", (req,res) => {
+	profile.handleProfileGet(req,res,db)
+});
+
+app.put("/image", (req, res) =>{
+	image.handleImage(req, res, db)
+});
+
+app.post("/imageurl", (req, res) =>{
+	image.handleApiCall(req, res)
+});
+
+app.listen(3001, ()=>{
+	console.log("Server Up port3001");
+});
+
+
+// const PORT = process.env.PORT
+// app.listen(PORT , ()=>{
+// 	console.log(`Server Up port ${PORT}`);
+// });
+
